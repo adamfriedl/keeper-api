17e1992c2653edece978e73c1e2490a5b87af353
Adam Friedl <adam.friedl@rakuten.com>
2020-01-16 13:21:37 -0800

    add login and register to nav

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => {
     return (
@@ -24,6 +25,7 @@ const App = () => {
                                     path='/register'
                                     component={Register}
                                 />
+                                <Route exact path='/login' component={Login} />
                             </Switch>
                         </div>
                     </Fragment>
